41af65afd9bd71d74052df3a246d18836e709ec2
meisteri <none>
2023-01-09 14:09:02 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# scuffed-markovbaj
+
+Run ```scuffed-markovbaj.py``` to use. It will play a sentence every 1-2 mminutes. Set the ```MARKOV_START``` and ```MARKOV_END``` enviroment variables to change frequency.
+
+Requirements: pygame, markovify, requests
---
+++ scuffed-markovbaj.py
@@ -0,0 +1,62 @@
+#!/bin/python
+from time import sleep
+import markovify
+import json
+import os.path
+from os import path
+import os
+import pygame.mixer
+from random import randint
+import requests as re
+import io
+
+class Model:
+    def __init__(self):
+        self.model = None
+        if path.exists("model.json"):
+            self.load_model()
+        else:
+            self.create_model()
+            self.write_model()
+ 
+    def load_model(self):
+        with open("model.json") as file:
+            self.model = markovify.Text.from_json(file.read())
+
+    def create_model(self):
+        with open("data.json") as file:
+            data = " ".join(json.loads(file.read()))
+        self.model = markovify.Text(data)
+        self.model.compile()
+        self.write_model()
+
+    def write_model(self):
+        # creating and compiling a model is slow so we save it to a file
+        model_json = self.model.to_json()
+        with open("model.json", "w") as file:
+            file.write(model_json)
+
+    def make_sentence(self, length: int):
+        return self.model.make_short_sentence(length)
+
+def main():
+    url = "https://api.streamelements.com/kappa/v2/speech?voice=Brian&text="
+    markov = Model()
+    pygame.mixer.init()
+    print("Running ...")
+    freq_start = os.getenv("MARKOV_START")
+    freq_end = os.getenv("MARKOV_END")
+    if freq_start is None:
+        freq_start = 60
+    if freq_end is None:
+        freq_end = 120
+    while True:
+        sentence = markov.make_sentence(300)
+        resp = re.get(url + sentence, stream=True)
+        sound_file = io.BytesIO(resp.content)
+        pygame.mixer.Sound(sound_file).play()
+        sleep(randint(freq_start, freq_end))
+
+if __name__ == "__main__":
+    main()
+
